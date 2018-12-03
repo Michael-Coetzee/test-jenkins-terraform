496d869671af4ac6e6fcaab20e693f60a3dbe526
Michael Coetzee <mcoetzee@yellowtail.co.za>
2018-12-03 18:07:06 +0200

    modify jenkinsfile

--- terra2.sh
+++ terra2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $vmss_rg
+
 cd terraform-plans/create-vmss-from-image
 
 terraform plan -out=tfplan -input=false -var 'terraform_resource_group='$vmss_rg -var 'terraform_vmss_name='$vmss_name -var 'terraform_azure_region='$location -var 'terraform_image_id='$image_id
